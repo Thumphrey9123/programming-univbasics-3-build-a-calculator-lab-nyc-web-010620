7c5f25b92b8bb3a884003f521e5ab3d4b34f0104
Thomas Humphrey <thumphrey9123@gmail.com>
2019-12-26 17:52:02 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,9 +1,9 @@
 def addition(num1, num2)
-
+  Math.sum(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  remainder = num1 - num2
 end
 
 def division(num1, num2)
@@ -19,5 +19,5 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
-
+  Math.sqrt(num)
 end
